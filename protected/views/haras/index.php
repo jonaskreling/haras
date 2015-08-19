e0861a0c505f372a1399b6cdfde339933e159307
jonasfrancokreling <jonasfrancokreling@gmail.com>
2015-08-19 13:50:53 -0300

    Correção da imagem oharas.jpg no index.php

--- protected/views/haras/index.php
+++ protected/views/haras/index.php
@@ -11,7 +11,7 @@
 		<div class="col-xs-12 col-md-4">
 			<h2>O HARAS</h2>
 			<div class="thumbnail venda">
-	      		<img src="<?php echo $baseUrlImage.'oharas.jpg'; ?>" alt="" class="image-border-dashed">
+	      		<img src="<?php echo $baseUrlImage.'oharas.jpg'; ?>" alt="" class="image-border-dashed" style="height:230px;overflow:hidden;">
 	      	</div>
 			<p class="text-left">O <b>Haras Aleixo</b> se destaca na criação e desenvolvimento da raça, produzindo uma enorme gama de campeões.</p>
 			<p class="text-right">
